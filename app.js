cc042d23282a6f0e52ba59906835dd2e1824dd2a
mmagdy2020 <mmagdy@mum.edu>
2020-04-03 22:27:17 -0500

    adding db connection and adjust folder to wrok with the new changes.

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const session = require('express-session');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const mongoose = require('mongoose')
+const MongoDBStore = require('connect-mongodb-session')(session)
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+const app = express();
+
+
+const store = new MongoDBStore({ // where I have to store my Data...
+  uri: process.env.mongoCLoud_url, // the URL for my DB  Bug: Uri instead of url
+  collection: 'dbSession' // will create a new Collection 
+})
+
+app.use(session({ secret: 'MySecret', resave: false, saveUninitialized: false, store: store })) //for session 2
+
 app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// view engine setup
+
+app.use(express.urlencoded({ extended: false }));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,4 +42,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+
+mongoose.connect(process.env.mongoCLoud_url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+.then(() => {
+  console.log("Connected to DB...")
+
+    app.listen(process.env.PORT || 3000)
+
+}).catch(err => console.log(err))
