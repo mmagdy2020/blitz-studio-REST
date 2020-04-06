a87a96adb94a5f81afb5a4d6a37464eaf62aff8f
mmagdy2020 <mmagdy@mum.edu>
2020-04-06 16:56:39 -0500

    add restApi for getById delete update and add imgUrl to classes ...

--- routes/classes.routes.js
+++ routes/classes.routes.js
@@ -3,8 +3,13 @@ const router = express.Router()
 
 const classController = require('../controllers/classes.controller')
 
+// MO - changing naming Convention as requested..
+router.get('/classes', classController.list);  
+router.post('/classes', classController.insert)
 
-router.get('/class', classController.list);
-router.post('/class', classController.insert)
+router.get('/classes/:classId',classController.getById)
+router.patch('/classes/:classId', classController.updateById);
+router.delete('/classes/:classId', classController.deleteById);
+
 
 module.exports = router;
