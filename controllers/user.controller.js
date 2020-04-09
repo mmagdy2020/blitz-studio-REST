f5b2d442d1ad2a289b1dc853c6a00e2a706859e7
Sophia Blitz <sophiablitz@gmail.com>
2020-04-09 11:27:35 -0500

    add Update and DeleteById, change authenticate path
    update: has no validation, need to check for change in email address to a valid and available email address
    authenticate is available from user/authenticate  (no longer from users/authenticate)
    user model: added a 'balance' property default 0. added imgUrl property, default to blitz-studio-icon.jpg

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,17 +1,23 @@
 const User = require('../models/user.model')
 
+
+/**
+* CREATE: insert user into database
+*/
 exports.insert = (req, res, next) => {
   console.log("insert: req.body", req.body)
   User.create(req.body)
     .then(result => {
-      console.log("insert: result",result);
+      console.log("insert: result", result);
       res.status(201).send(result);
     })
     .catch(err => {
       res.status(500).send({ errMsg: err });
     });
 };
-
+/**
+* READ: get all users in database
+*/
 exports.list = (req, res, next) => {
   User.find()
     .then(users => {
@@ -21,6 +27,9 @@ exports.list = (req, res, next) => {
       res.status(500).send({ errMsg: err });
     });
 }
+/**
+* READ: get a user by ID in database
+*/
 exports.findById = (req, res, next) => {
   User.findById(req.params.id)
     .then(user => {
@@ -30,12 +39,17 @@ exports.findById = (req, res, next) => {
     .catch(err => {
       res.status(500).send({ errMsg: err });
     });
-
 }
-exports.authenticate = (req, res, next)=>{
-  User.findOne({password: req.body.password, email: req.body.email})
+
+/**
+* READ: with filter
+* authenticate user with email and password
+* returns: If user is authenticated, return user, else error.
+*/
+exports.authenticate = (req, res, next) => {
+  User.findOne({ password: req.body.password, email: req.body.email })
     .then(user => {
-      console.log("authenticate user found:",user);
+      console.log("authenticate user found:", user);
       res.status(200).send(user);
     })
     .catch(err => {
@@ -43,13 +57,46 @@ exports.authenticate = (req, res, next)=>{
     });
 
 }
-exports.check = (req, res, next)=> {
+/**
+* CHECK:
+* determine if username/email already exists in database
+*/
+exports.check = (req, res, next) => {
   User.findOne({ email: req.body.email })
     .then(user => {
       // console.log("does user exist:", user ? true : false);
-      res.status(200).send({userExists: user?true:false});
+      res.status(200).send({ userExists: user ? true : false });
     })
     .catch(err => {
       res.status(500).send({ errMsg: err });
     });
 }
+
+/**
+* UPDATE: update user in database
+*/
+exports.update = (req, res, next) => {
+  console.log("update: req.body", req.body);
+  User.findByIdAndUpdate({ _id: req.body._id }, req.body)
+    .then(result => {
+      console.log("update: result", result);
+      res.status(201).send(result);
+    })
+    .catch(err => {
+      res.status(500).send({ errMsg: err });
+    });
+};
+/**
+* DELETE: delete user in database
+*/
+exports.deleteById = (req, res, next) => {
+  console.log("delete: req.body", req.body);
+  User.findByIdAndDelete(req.params.id)
+    .then(result => {
+      console.log("delete: result", result);
+      res.status(201).send(result);
+    })
+    .catch(err => {
+      res.status(500).send({ errMsg: err });
+    });
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,10 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 const userController = require('../controllers/user.controller')
-router.post('/users/authenticate', userController.authenticate);
 router.get('/users', userController.list);
-router.post('/user/exists', userController.check)
-router.get('/user/:id', userController.findById)
-router.post('/user', userController.insert)
+
+router.post('/user/authenticate', userController.authenticate);
+router.post('/user/exists', userController.check);
+router.get('/user/:id', userController.findById);
+router.delete('/user/:id', userController.deleteById);
+router.post('/user', userController.insert);
+router.patch('/user', userController.update);
 
 module.exports = router;
