e01f3546d635fef0888dceb91b2f4c76eea24790
Mahmoud Magdy <mahmoudmagdy@Mahmouds-MBP.attlocal.net>
2020-04-13 11:57:21 -0500

    Fixing Patch API

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,9 @@ const express = require('express');
 const router = express.Router();
 const cors = require('cors');
 /* GET home page. */
-<<<<<<< Updated upstream
 router.get('/', function(req, res, next) {
   console.log(req.session)
   res.render('index', { title: 'Express' });
-=======
-router.get('/', cors(), function(req, res, next) {
-  res.status(200).json({ "title": "Hello react! this is from the REST project index route. CORS enabled" });
->>>>>>> Stashed changes
 });
 
 module.exports = router;
